9efca81f723b4169c3b2854fba3da76764b8c92b
lennafebriana12 <lennahutapea18@gmail.com>
2024-10-18 11:10:32 +0700

    submit register

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.auth')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">Register</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{
+route('post.register') }}">
+                        @csrf
+
+                        <div class="form-group mb-2">
+                            <label for="name">Full Name</label>
+                            <input id="name" type="text"
+                                   class="form-control
+
+@error('name') is-invalid @enderror" name="name"
+                                     value="{{ old('name') }}"
+
+required autofocus>
+                            @error('name')
+                            <span class="invalid-feedback"
+role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-2">
+                            <label for="email">Email Address</label>
+                            <input id="email" type="email"
+                                class="form-control @error('email')
+is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required>
+                            @error('email')
+                                <span class="invalid-feedback"
+role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        
+                        <div class="form-group mb-2">
+                            <label for="password">Password</label>
+                            <input id="password" type="password"
+                                class = "form-control
+@error('password') is-invalid @enderror" name="password" required>
+                            @error('password')
+                                <span class="invalid-feedback"
+role = "alret">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="text-end"
+                        <button type="submit" class="btn btn-
+primary">
+                            List
+                    </button>
+
+                </div>
+                <hr />
+                <div class="mb-2 text-center">
+                    <span> sudah memiliki akun? <a href = "{{ route('login') }}">masuk disini</a></span>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+@endsection
